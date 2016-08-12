34eae0b6bc756fbc73235fc926b6d3671a8289c7
EJSohn <5on.3un.joo@gmail.com>
2016-08-12 15:50:06 +0900

    Chapter-1 Number 3 2016.08.12

---
+++ Chapter-1/Number-3.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using std::endl;
+using std::cout;
+
+template<class T>
+void fill(int start, int end, T value)
+{
+    T a[end];
+    for(int i=start; i<end-1; i++){
+        a[i]=value;
+    }
+    
+    cout<<"[";
+    for(int i=0; i<end; i++){
+        cout<<a[i];
+        cout<<",";
+    }
+    cout<<"]"<<endl;
+}
+
+int main(){
+    fill(0, 5, 1.0);
+
+    return 0;
+}
+
+
