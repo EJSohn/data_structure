0331b12c547b5d574a9a5c677562c4e7db64b0e5
EJSohn <5on.3un.joo@gmail.com>
2016-09-01 16:57:34 +0900

    add add, increment, ouput functions

--- Chapter-1/Section1.5-1.9/Number-16.cpp
+++ Chapter-1/Section1.5-1.9/Number-16.cpp
@@ -53,7 +53,39 @@ void currency::setValue(double theAmount)
     cents = (unsigned int) ((theAmount + 0.001 - dollars)*100);
 }
 
+currency currency::add(const currency& x) const
+{
+    long a1, a2, a3;
+    currency result;
+    
+    a1 = dollars * 100 + cents;
+    if (sign == minus) a1 = -a1;
 
+    a2 = x.dollars * 100 + x.cents;
+    if (x.sign==minus) a2 = -a2;
+
+    a3 = a1 + a2;
+
+    if (a3<0) {result.sign = minus; a3 = -a3;}
+    result.dollars = a3/100;
+    result.cents = a3 - result.dollars * 100;
+
+    return result;
+}
+
+currency& currency::increment(const currency& x)
+{
+    *this = add(x);
+    return *this;
+}
+
+void currency::output() const
+{
+    if (sign==minus) cout << '-';
+    cout << '$' << dollars << '.';
+    if (cents < 10) cout << '0';
+    cout << cents;
+}
 
 
 
