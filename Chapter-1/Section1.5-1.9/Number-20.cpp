e7a43959348bdaee7fda97145b5c170b3511ab34
EJSohn <5on.3un.joo@gmail.com>
2016-09-06 11:34:11 +0900

    Chapter1/Number-20-(c) 16.9.6

--- Chapter-1/Section1.5-1.9/Number-20.cpp
+++ Chapter-1/Section1.5-1.9/Number-20.cpp
@@ -4,9 +4,31 @@ using namespace std;
 
 unsigned int Fibonacci(unsigned int n)
 {
+    // use recursion
+
     if(n<2) return n;
     else return Fibonacci(n-1)+Fibonacci(n-2);
 }
+
+unsigned int FibonacciV2(unsigned int n)
+{
+    // not use recursion
+
+    // 동적 할당 
+    unsigned int* result = new unsigned int[n];
+    
+    result[0] = 0;
+    result[1] = 1;
+
+    for( int i=2; i<n+1; i++ )
+    {
+        result[i] = result[i-2] + result[i-1];
+    }
+
+    return result[n];
+
+}
+
 
 int main()
 {
@@ -18,6 +40,9 @@ int main()
     for (int i=0; i<=n; i++) { cout<<Fibonacci(i)<<", "; }
     cout<<"]"<<endl;
     
+    cout<<"use non-recursive fibonacci function: "<<endl;
+    cout<<FibonacciV2(n)<<endl;
+
     return 0;
 }
 
