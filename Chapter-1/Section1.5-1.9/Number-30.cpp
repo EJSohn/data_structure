55f86e136c5d9d5ef7ef5685648d01736b0fa5e6
EJSohn <5on.3un.joo@gmail.com>
2016-09-13 23:35:20 +0900

    16.9.13 fix bug

--- Chapter-1/Section1.5-1.9/Number-30.cpp
+++ Chapter-1/Section1.5-1.9/Number-30.cpp
@@ -4,27 +4,58 @@
 using namespace std;
 
 template <class T>
-void permutations(T list[], int k, int m)
-{
-    // sorting the list elements into ascending order
-    sort(list+k-1, list+m+1);
+void printArr(T list[], int m);
 
-    do {
-        copy(list+k-1, list+m+1,
-                ostream_iterator<T>(cout, ""));
-        cout<<endl;
-    }
-    while (next_permutation(list+k-1, list+m+1));
-}
+template <class T>
+void permutations(T list[], int m);
+
 
 int main()
 {
-    char list[] = {'a', 'b', 'c'};
+    int list[] = {3, 1, 2};
 
-    permutations(list, 1, 3);
+    permutations(list, 2);
+
+    char clist[] = {'d', 'f', 'a'};
+
+    permutations(clist, 2);
 
     return 0;
 }
 
+template <class T>
+void printArr(T list[], int m)
+{
+    cout<<"{";
 
+    for(int i=0; i<m+1; i++)
+    {
+        cout<<list[i]<<" ";
+    }
+
+    cout<<"}"<<endl;
+}
+
+
+template <class T>
+void permutations(T list[], int m)
+{
+    // before sorting
+    cout<<"before: ";
+    printArr(list, m);
+
+    // sorting the list elements into ascending order
+    sort(list, list+m+1);
+
+    // after sorting
+    cout<<"after: ";
+    printArr(list, m);
+
+    do {
+        copy(list, list+m+1,
+                ostream_iterator<T>(cout, ""));
+        cout<<endl;
+    }
+    while (next_permutation(list, list+m+1));
+}
 
